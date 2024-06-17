d3301632e401b4d669ec76398f76df1ec6aa2f05
Abinesh <abineshkannan98@gmail.com>
2024-06-18 00:25:49 +0530

    login inital commit

---
+++ commonFunction.js
@@ -0,0 +1,48 @@
+/* Common links and script tags */
+document.addEventListener('DOMContentLoaded', () => {
+    let bootstrapLinks = `
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    `;
+    document.head.insertAdjacentHTML('beforeend', bootstrapLinks);
+
+    /* Checl user login status. */
+    (async (token) => {
+        if (!token) {
+            changeRoutesFunction('/index.html')
+            return '';
+        };
+
+        let { data: { status } } = await axios.post('http://192.168.1.15:3000/api/v1/checkLoginStatus', { token: token.substring(1) });
+
+        if (status == 200) changeRoutesFunction('/superAdmin.html');
+        else changeRoutesFunction('/index.html');
+
+    })(checkUserLoginStatus('id_token'));
+});
+
+/* Function to change the routes. */
+const changeRoutesFunction = (pathName) => { if (window.location.pathname != pathName) window.location.href = `.${pathName}` };
+
+/* Check user login status. */
+const checkUserLoginStatus = (cookieName) => {
+    const name = cookieName;
+    const splitCookie = document.cookie.split(';');
+    for (let index = 0; index < splitCookie.length; index++) {
+        let element = splitCookie[index].trim();
+        if (element.indexOf(name) === 0) return element.substring(name.length, element.length);
+    };
+};
+
+/* Toaster */
+function showToast(message, isError) {
+    const toastContainer = document.getElementById('toast-container');
+    const toast = document.createElement('div');
+    toast.className = 'toast';
+    toast.textContent = message;
+    !isError ? toast.style.backgroundColor = "rgb(23, 147, 23)" : toast.style.backgroundColor = "#df5959";
+
+    toastContainer.appendChild(toast);
+    setTimeout(() => { toast.remove(); }, 3000);
+};
--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-(async () => {
-    let responsse = await fetch('https://api.restful-api.dev/objects', {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        }
+const emailId = document.getElementById('login-emailId');
+const password = document.getElementById("login-password");
+const showPassword = document.getElementById('show-password-checkbox');
+const loginSubmitBtn = document.getElementById('login-submitbtn');
+
+emailId.value = 'abineshkannan98@gmail.com';
+password.value = 'Qwerty@123';
+
+showPassword?.addEventListener('change', () => {
+    if (showPassword.checked) password.type = 'text';
+    else password.type = 'password';
+});
+
+
+/* Login Action */
+loginSubmitBtn.onclick = async () => {
+    if (!emailId.value || !password.value) {
+        showToast('Please enter the required fields', true);
+        return '';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(emailId.value)) {
+        showToast('Please enter valid email address', true);
+        return '';
+    } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/i.test(password?.value)) {
+        showToast('Please provide password in correct format (It should atleast have one capital letter, one special characters, one numbers and the length of the password should me more than 8 characters.)', true);
+        return '';
+    };
+
+    let { data, data: { status } } = await axios.post('http://192.168.1.15:3000/api/v1/login', {
+        emailId: emailId.value,
+        password: password.value
     });
 
-    let resp = await responsse.json();
-    console.log(resp, 'respresprespresprespresp')
-})();
+    if (status == 200) {
+        const date = new Date();
+        date.setTime(date.getTime() + (3 * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = `id_token=${data?.token}; expires=${expires}`;
+        window.location.href = './superAdmin.html';
+    } else {
+        showToast('Something went wrong in login. Please try again later', true);
+        return '';
+    };
+
+};
